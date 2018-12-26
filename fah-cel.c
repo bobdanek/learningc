a7c2488aef7a88110421b0ce6765e0c0d8a013e8
Bob Danek <rdk@aux.sh>
2018-12-26 15:22:09 -0800

    Exercise 1-4 completed

--- fah-cel.c
+++ fah-cel.c
@@ -2,7 +2,8 @@
 
 /* print Fahrenheit-Celsius table
     for fahr =, 20, ..., 300 */
-int main(void)
+
+void print_f_to_c(void)
 {
   float fahr, celsius;
   int lower, upper, step;
@@ -12,9 +13,39 @@ int main(void)
   step = 20;        /* step size */
 
   fahr = lower;
+
+  printf("%3s %6s\n", "F", "C");
+
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
+}
+
+void print_c_to_f(void)
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;        /* lower limit of temperature table */
+  upper = 150;      /* upper limit */
+  step = 10;        /* step size */
+
+  celsius = lower;
+
+  printf("%3s %6s\n", "C", "F");
+
+  while (celsius <= upper) {
+    fahr = celsius * (9.0 / 5.0) + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
+
+int main(void)
+{
+  print_f_to_c();
+  print_c_to_f();
+  return 0;
 }
