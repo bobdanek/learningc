b08dfa695aed618ed1346d2c34e32b97c427690f
Bob Danek <rdk@aux.sh>
2018-12-28 13:21:01 -0800

    Exercise 1-7 done

--- filecopying.c
+++ filecopying.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/* copy input to output; 1st version */
+/* copy input to output; 2nd version */
 int main(void)
 {
   int c;
 
-  c = getchar();
-  while (c != EOF) {
+  printf("getchar() !=EOF is: %d\n", (getchar() != EOF));
+  printf("EOF is: %d\n", EOF);
+
+  while ((c = getchar()) != EOF)
     putchar(c);
-    c = getchar();
-  }
 }
